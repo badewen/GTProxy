2c03eb391e42e73fd05a5bcc2b60690c12d16c73
Badewen <richie.serevanus3@gmail.com>
2023-09-03 17:44:55 +0700

    added more packet event. also help i kept using macro, sorry :crying_face:.

--- src/eventmanager/event_manager.cpp
+++ src/eventmanager/event_manager.cpp
@@ -1,14 +1,88 @@
 #include "event_manager.h"
 
+using namespace event_manager;
+
+// send help, i am addicted to using macro
+
+#define GTPROXY_HANDLE_LIST_DECLARE(event_name) std::unordered_map<std::string, \
+                                                event_name##CbHandle> \
+                                                event_name##HandleList {}
+
+// im lazy
+#define GTPROXY_OUT_PARAM_TO_RETURN(code)   \
+        user_data out {};                   \
+        out.block_packet = false;          \
+        code                                \
+        return out.block_packet;
 
 eventpp::CallbackList<event_manager::OnWorldEnter> OnWorldEnterList {};
-std::unordered_map<std::string, event_manager::OnWorldEnterCbHandle> OnWorldEnterHandleList {};
+GTPROXY_HANDLE_LIST_DECLARE(OnWorldEnter);
 
-void event_manager::AddOnWorldEnter(event_manager::OnWorldEnter cb, const std::string& id) {
+std::function<event_manager::OnReceivePacket> OnReceivePacketCb = nullptr;
+
+eventpp::CallbackList<event_manager::OnReceiveTankPacket> OnReceiveTankPacketList {};
+GTPROXY_HANDLE_LIST_DECLARE(OnReceiveTankPacket);
+
+eventpp::CallbackList<event_manager::OnReceiveRawPacket> OnReceiveRawPacketList {};
+GTPROXY_HANDLE_LIST_DECLARE(OnReceiveRawPacket);
+
+eventpp::CallbackList<event_manager::OnReceiveVariantlist> OnReceiveVariantlistList {};
+GTPROXY_HANDLE_LIST_DECLARE(OnReceiveVariantlist);
+
+
+void event_manager::AddOnWorldEnter(std::function<event_manager::OnWorldEnter> cb, const std::string& id) {
     auto h = OnWorldEnterList.append(cb);
     OnWorldEnterHandleList.insert_or_assign(id, h);
 }
 
 void event_manager::InvokeOnWorldEnter(const World& world) {
     OnWorldEnterList(world);
+}
+
+
+void event_manager::SetOnReceivePacket(std::function<event_manager::OnReceivePacket> cb) {
+    OnReceivePacketCb = cb;
+}
+
+bool event_manager::InvokeOnReceivePacket(ENetPeer* peer, ENetPacket* packet) {
+    GTPROXY_OUT_PARAM_TO_RETURN({
+        OnReceivePacketCb(peer, packet, &out);
+    });
+}
+
+
+void event_manager::AddOnReceiveTankPacket(std::function<event_manager::OnReceiveTankPacket> cb, const std::string &id) {
+    auto h = OnReceiveTankPacketList.append(cb);
+    OnReceiveTankPacketHandleList.insert_or_assign(id, h);
+}
+
+bool
+event_manager::InvokeOnReceiveTankPacket(ENetPeer *peer, player::GameUpdatePacket *packet) {
+    GTPROXY_OUT_PARAM_TO_RETURN({
+        OnReceiveTankPacketList(peer, packet, &out);
+    });
+}
+
+
+void event_manager::AddOnReceiveRawPacket(std::function<OnReceiveRawPacket> cb, const std::string &id) {
+    auto h = OnReceiveRawPacketList.append(cb);
+    OnReceiveRawPacketHandleList.insert_or_assign(id, h);
+}
+
+bool event_manager::InvokeOnReceiveRawPacket(ENetPeer *peer, ENetPacket *packet) {
+    GTPROXY_OUT_PARAM_TO_RETURN({
+        OnReceiveRawPacketList(peer, packet, &out);
+    });
+}
+
+
+void event_manager::AddOnReceiveVariantlist(std::function<OnReceiveVariantlist> cb, const std::string &id) {
+    auto h = OnReceiveVariantlistList.append(cb);
+    OnReceiveVariantlistHandleList.insert_or_assign(id, h);
+}
+
+bool event_manager::InvokeOnReceiveVariantlist(ENetPeer *peer, VariantList* packet) {
+    GTPROXY_OUT_PARAM_TO_RETURN({
+       OnReceiveVariantlistList(peer, packet, &out);
+    });
 }
--- src/eventmanager/event_manager.h
+++ src/eventmanager/event_manager.h
@@ -3,12 +3,60 @@
 #include "eventpp/callbacklist.h"
 
 #include "../world/world.h"
+#include "../network/packet.h"
+#include "enet/enet.h"
+#include "../proton/shared/util/Variant.h"
+
+#define GTPROXY_CB_HANDLE_DECLARE(cb_type) using cb_type##CbHandle = eventpp::internal_::CallbackListBase<cb_type,\
+                                            eventpp::DefaultPolicies>::Handle
 
 namespace event_manager
 {
-    using OnWorldEnter = void(const World& world);
-    using OnWorldEnterCbHandle = eventpp::internal_::CallbackListBase<event_manager::OnWorldEnter, eventpp::DefaultPolicies>::Handle;
 
-    void AddOnWorldEnter(OnWorldEnter cb, const std::string& id);
+    struct user_data {
+        // this is used by OnReceivePacket* and OnSendPacket* functions
+        // if set, will block packet from going to gt client or sent to the server
+        bool block_packet;
+
+        // who knows what you want to put inside here.
+        void* data;
+    };
+
+    using OnWorldEnter = void(const World& world);
+    GTPROXY_CB_HANDLE_DECLARE(OnWorldEnter);
+
+    void AddOnWorldEnter(std::function<OnWorldEnter> cb, const std::string& id);
     void InvokeOnWorldEnter(const World& world);
+
+
+    using OnReceivePacket = void(ENetPeer* peer, ENetPacket* packet, user_data* data);
+
+    void SetOnReceivePacket(std::function<OnReceivePacket> cb);
+    bool InvokeOnReceivePacket(ENetPeer* peer, ENetPacket* packet);
+
+
+    //these one is an actual event.
+    using OnReceiveTankPacket = void(ENetPeer* peer, player::GameUpdatePacket* packet, user_data* data);
+    GTPROXY_CB_HANDLE_DECLARE(OnReceiveTankPacket);
+
+    void AddOnReceiveTankPacket(std::function<OnReceiveTankPacket> cb, const std::string& id);
+
+    bool InvokeOnReceiveTankPacket(ENetPeer* peer, player::GameUpdatePacket* packet);
+
+
+    using OnReceiveRawPacket = void(ENetPeer* peer, ENetPacket* packet, user_data* data);
+    GTPROXY_CB_HANDLE_DECLARE(OnReceiveRawPacket);
+
+    void AddOnReceiveRawPacket(std::function<OnReceiveRawPacket> cb, const std::string& id);
+
+    bool InvokeOnReceiveRawPacket(ENetPeer* peer, ENetPacket* packet);
+
+
+    using OnReceiveVariantlist = void(ENetPeer* peer, VariantList* packet, user_data* data);
+    GTPROXY_CB_HANDLE_DECLARE(OnReceiveVariantlist);
+
+    void AddOnReceiveVariantlist(std::function<OnReceiveVariantlist> cb, const std::string& id);
+
+    bool InvokeOnReceiveVariantlist(ENetPeer* peer, VariantList* packet);
+
 };

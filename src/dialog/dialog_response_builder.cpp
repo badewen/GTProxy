23b3ab3f403901fe899e9dcd5aef5a4f624da560
Badewen <richie.serevanus3@gmail.com>
2023-11-26 16:32:37 +0700

    add DialogResponseBuilder class. it is a wrapper to respond to a dialog request.

---
+++ src/dialog/dialog_response_builder.cpp
@@ -0,0 +1,61 @@
+#include "dialog_response_builder.h"
+
+using namespace dialog;
+
+DialogResponseBuilder::DialogResponseBuilder(const std::string& dialog_request)
+    : m_text_inputs {}
+    , m_data_embeds {}
+    , m_dialog_name {}
+    {
+        for (const auto &raw_dialog_data: utils::TextParse{dialog_request}.get_all_array()) {
+            std::vector<std::string> dialog_data_tokenize = utils::TextParse::string_tokenize(raw_dialog_data);
+
+            if (dialog_data_tokenize.at(0) == "embed_data") {
+                m_data_embeds.push_back(
+                        raw_dialog_data.substr(
+                                strlen("embed_data"),
+                                raw_dialog_data.length() - strlen("embed_data")
+                        ) + "|"
+                );
+            } else if (dialog_data_tokenize.at(0) == "add_text_input" ||
+                       dialog_data_tokenize.at(0) == "add_checkbox"
+                    ) {
+                m_text_inputs.insert_or_assign(dialog_data_tokenize.at(1), dialog_data_tokenize.at(3));
+            } else if (dialog_data_tokenize.at(0) == "end_dialog") {
+                m_dialog_name = dialog_data_tokenize.at(1);
+            } else if (dialog_data_tokenize.at(0) == "add_button") {
+                m_button_list.push_back(dialog_data_tokenize.at(1));
+            }
+    }
+}
+
+void DialogResponseBuilder::input_text(const std::string& input_text_id, const std::string& value) {
+    try {
+        m_text_inputs.at(input_text_id) = value + "|";
+    }
+    catch (...) {
+        return;
+    }
+}
+
+std::string DialogResponseBuilder::build() {
+    std::string ret {};
+
+    ret += "action|dialog_return\n";
+    ret += "dialog_name|" + m_dialog_name + "\n";
+
+    for (const auto& data : m_data_embeds) {
+        ret += data + "\n";
+    }
+
+    if (!m_clicked_button.empty()) {
+        ret += "buttonClicked|" + m_clicked_button;
+    }
+
+    for (const auto& input : m_text_inputs) {
+        ret += input.first + "|" + input.second;
+    }
+
+    return ret;
+
+}
---
+++ src/dialog/dialog_response_builder.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+#include <utility>
+
+#include "../utils/text_parse.h"
+
+namespace dialog {
+
+class DialogResponseBuilder {
+public:
+    explicit DialogResponseBuilder(const std::string& dialog_request);
+
+    void input_text(const std::string& input_text_id, const std::string& value);
+
+    std::string build();
+
+
+    void click_button(std::string button_id) {
+        m_clicked_button = std::move(button_id);
+    }
+
+    void enable_checkbox(const std::string& checkbox_id, bool enable) {
+        input_text(checkbox_id, std::to_string(enable));
+    }
+
+    std::string get_dialog_name() { return m_dialog_name; }
+
+    bool has_button(const std::string& id) {
+        return std::find(m_button_list.begin(), m_button_list.end(), id) != m_button_list.end();
+    }
+    bool has_text_input(const std::string& id) { return m_text_inputs.find(id) != m_text_inputs.end(); }
+    bool has_data_embed(const std::string& embed_data_key) {
+        return std::find(m_data_embeds.begin(), m_data_embeds.end(), embed_data_key) != m_data_embeds.end();
+    }
+
+private:
+    // includes text input and checkbox
+    std::unordered_map<std::string, std::string> m_text_inputs;
+    std::vector<std::string> m_data_embeds;
+    std::vector<std::string> m_button_list;
+    std::string m_clicked_button;
+    std::string m_dialog_name;
+};
+}

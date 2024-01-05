d8ec258933da4bfe4debc975845fa7432db46c69
Badewen <richie.serevanus3@gmail.com>
2024-01-05 16:25:46 +0700

    update module

--- src/module/modules/cheats/auto_fish_module.cpp
+++ src/module/modules/cheats/auto_fish_module.cpp
@@ -1,35 +1,47 @@
 #include "auto_fish_module.h"
 
-#include "../../../client/client.h"
+#include "../../../network/server/server.h"
+#include "../necessary/net_avatar_handler_module.h"
 
 using namespace modules;
 
 void AutoFishModule::on_enable() {
-    m_client->get_ctx()->OnOutgoingTankPacket.Register("AutoFish_Module", &AutoFishModule::on_outgoing_tile_hook, this);
-    m_client->get_ctx()->OnIncomingTankPacket.Register("AutoFish_Module", &AutoFishModule::on_gone_fishing_hook, this);
-    m_client->get_ctx()->OnIncomingVarlist.Register("AutoFish_Module", &AutoFishModule::on_splash_audio_hook, this);
+    m_proxy_server->add_on_outgoing_tank_packet_callback("AutoFish_Module", &AutoFishModule::on_outgoing_tile_hook, this);
+    m_proxy_server->get_client()->add_on_incoming_tank_packet_callback("AutoFish_Module", &AutoFishModule::on_gone_fishing_hook, this);
+    m_proxy_server->get_client()->add_on_incoming_varlist_packet_callback("AutoFish_Module", &AutoFishModule::on_splash_audio_hook, this);
 }
 
 void AutoFishModule::on_disable() {
-    m_client->get_ctx()->OnOutgoingTankPacket.Remove("AutoFish_Module");
-    m_client->get_ctx()->OnIncomingTankPacket.Remove("AutoFish_Module");
-    m_client->get_ctx()->OnIncomingVarlist.Remove("AutoFish_Module");
+    m_proxy_server->remove_on_outgoing_tank_packet_callback("AutoFish_Module");
+    m_proxy_server->get_client()->remove_on_incoming_tank_packet_callback("AutoFish_Module");
+    m_proxy_server->get_client()->remove_on_incoming_varlist_packet_callback("AutoFish_Module");
 
     m_fishing_packet.reset();
     m_tile_packet.reset();
     m_fishing_packet.reset();
 }
 
-void AutoFishModule::on_outgoing_tile_hook(const packet::GameUpdatePacket *tank_packet, bool *forward_packet) {
+void AutoFishModule::on_outgoing_tile_hook(
+        const packet::GameUpdatePacket *tank_packet,
+        std::shared_ptr<peer::Peer> gt_peer,
+        bool *forward_packet
+) {
     if (tank_packet->type == packet::PACKET_TILE_CHANGE_REQUEST) {
         m_tile_packet = *tank_packet;
     }
 }
 
-void AutoFishModule::on_gone_fishing_hook(const packet::GameUpdatePacket* tank_packet, bool* forward_packet) {
+void AutoFishModule::on_gone_fishing_hook(
+        const packet::GameUpdatePacket* tank_packet,
+        std::shared_ptr<peer::Peer> gt_server_peer,
+        bool* forward_packet
+) {
+    std::shared_ptr<NetAvatarHandlerModule> net_avatar_module =
+            m_proxy_server->get_module_manager()->get_module_by_name<NetAvatarHandlerModule>("NetAvatarHandler_Module");
+
     if (tank_packet->type == packet::PACKET_GONE_FISHIN &&
         m_tile_packet.has_value() &&
-        tank_packet->net_id == m_client->get_ctx()->PlayerInfo.NetID)
+        tank_packet->net_id == net_avatar_module->get_current_net_avatar()->NetID)
     {
         m_fishing_packet = m_tile_packet;
         m_fish_out_packet = m_fishing_packet;
@@ -39,8 +51,16 @@ void AutoFishModule::on_gone_fishing_hook(const packet::GameUpdatePacket* tank_p
     }
 }
 
-void AutoFishModule::on_splash_audio_hook(VariantList* varlist, int32_t net_id, bool* forward_packet) {
-    if (net_id != m_client->get_ctx()->PlayerInfo.NetID &&
+void AutoFishModule::on_splash_audio_hook(
+        VariantList* varlist,
+        int32_t net_id,
+        std::shared_ptr<peer::Peer> gt_server_peer,
+        bool* forward_packet
+) {
+    std::shared_ptr<NetAvatarHandlerModule> net_avatar_module =
+            m_proxy_server->get_module_manager()->get_module_by_name<NetAvatarHandlerModule>("NetAvatarHandler_Module");
+
+    if (net_id != net_avatar_module->get_current_net_avatar()->NetID &&
         !m_fishing_packet.has_value())
     {
         return;
@@ -54,11 +74,10 @@ void AutoFishModule::on_splash_audio_hook(VariantList* varlist, int32_t net_id, 
         return;
     }
 
-    if (m_client->get_ctx()->PlayerInfo.NetID != net_id) {
-        m_client->log_to_client("Not out splash packet");
+    if (net_avatar_module->get_current_net_avatar()->NetID != net_id) {
         return;
     }
 
-    m_client->queue_packet(player::Peer::build_raw_packet(&m_fishing_packet.value()), true);
-    m_client->queue_packet_delayed(player::Peer::build_raw_packet(&m_fishing_packet.value()), true, 250);
+    m_proxy_server->get_client()->send_to_gt_server(packet::create_raw_packet(&m_fishing_packet.value()), true);
+    m_proxy_server->get_client()->send_to_gt_server_delayed(packet::create_raw_packet(&m_fishing_packet.value()), true, 250);
 }
--- src/module/modules/cheats/auto_fish_module.h
+++ src/module/modules/cheats/auto_fish_module.h
@@ -3,6 +3,7 @@
 #include "../module_base.h"
 
 #include <optional>
+#include <memory>
 
 #include "proton/Variant.h"
 #include "enet/include/enet/enet.h"
@@ -10,21 +11,38 @@
 #include "../../../packet/packet.h"
 #include "../../../utils/text_parse.h"
 
+namespace peer {
+class Peer;
+}
+
 namespace modules {
 
 class AutoFishModule : public module::ModuleBase {
 public:
-    AutoFishModule(client::Client* client)
-        : module::ModuleBase("AutoFish_Module")
+    explicit AutoFishModule(server::Server* proxy_server)
+        : module::ModuleBase("AutoFish_Module", {"NetAvatarHandler_Module"})
     {}
 
     void on_enable() override;
     void on_disable() override;
 
 private:
-    void on_outgoing_tile_hook(const packet::GameUpdatePacket* tank_packet, bool* forward_packet);
-    void on_gone_fishing_hook(const packet::GameUpdatePacket* tank_packet, bool* forward_packet);
-    void on_splash_audio_hook(VariantList* varlist, int32_t net_id, bool* forward_packet);
+    void on_outgoing_tile_hook(
+            const packet::GameUpdatePacket* tank_packet,
+            std::shared_ptr<peer::Peer> gt_peer,
+            bool* forward_packet
+    );
+    void on_gone_fishing_hook(
+            const packet::GameUpdatePacket* tank_packet,
+            std::shared_ptr<peer::Peer> gt_server_peer,
+            bool* forward_packet
+    );
+    void on_splash_audio_hook(
+            VariantList* varlist,
+            int32_t net_id,
+            std::shared_ptr<peer::Peer> gt_server_peer,
+            bool* forward_packet
+    );
 
 private:
     std::optional<packet::GameUpdatePacket> m_fishing_packet;
